811a36acc8da30abbe40e12df999bc7cbe896f93
cbruna <rbruna@fi.uba.ar>
2026-03-31 18:07:24 -0300

    lecture 2 homework

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,7 +2,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 1000
+//#define SLEEP_TIME_MS 1000
+
+/*student CRISTIAN BRUÑA modification:*/
+#define SLEEP_TIME_MS 250 
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
